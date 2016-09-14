716353184c7455fe8fa1d384ddeba0e471945721
Gabriel Bretschner <software@kanedo.net>
2016-09-14 14:33:38 +0200

    Initial Commit

---
+++ KeychainFramework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  KeychainFramework
+//
+//  Created by Gabriel Bretschner on 14.09.16.
+//  Copyright © 2016 Gabriel Bretschner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
